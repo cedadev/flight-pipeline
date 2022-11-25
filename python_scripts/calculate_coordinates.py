5c6386543ce40fb4a6a2833539d7a020e2fd315a
dwest77a <daniel.westwood@stfc.ac.uk>
2022-11-25 15:26:58 +0000

    Updated python files with existing code

---
+++ python_scripts/calculate_coordinates.py
@@ -0,0 +1,9 @@
+'''
+ --- Coordinate Calculations ---
+  - Find netcdf/hdf files
+  - Extract lat/lon coordinates
+  - Filter by 3SD, set max number of coords?
+'''
+
+if __name__ == '__main__':
+    print(__file__)
---
+++ python_scripts/find_existing_flights.py
@@ -0,0 +1,8 @@
+'''
+ --- Find Existing Flights ---
+  - Extract all items from flight finder index by organisation
+  - Save flight numbers to array - return array
+'''
+
+if __name__ == '__main__':
+    print(__file__)
