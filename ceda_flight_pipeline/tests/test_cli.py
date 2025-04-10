8f7495fea44b48d3e27593c9d341dee281244bd7
ioana.circu@stfc.ac.uk <ioana.circu@stfc.ac.uk>
2025-04-10 15:42:28 +0100

    added test for imports
    - used the black python formatter for the files inside tests dir

--- ceda_flight_pipeline/tests/test_cli.py
+++ ceda_flight_pipeline/tests/test_cli.py
@@ -20,14 +20,19 @@ class TestFlightUpdate(unittest.TestCase):
 
     # Test openConfig() with the correct configuration
     # Mock opening file
-        
-    @patch("builtins.open", new_callable=mock_open, read_data="line0\n/flights-dir\nline2\n/archive\nline4\n")
-    @patch.dict(os.environ, {"CONFIG_FILE": "dummy_config"})  # Set the CONFIG_FILE env variable
 
-
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data="line0\n/flights-dir\nline2\n/archive\nline4\n",
+    )
+    @patch.dict(
+        os.environ, {"CONFIG_FILE": "dummy_config"}
+    )  # Set the CONFIG_FILE env variable
     def test_open_config_success(self, mock_file):
 
         from ceda_flight_pipeline.cli import openConfig
+
         root, archive = openConfig()
 
         # Check if the function correctly reads and returns the expected paths
@@ -35,11 +40,13 @@ class TestFlightUpdate(unittest.TestCase):
         self.assertEqual(archive, "/archive")
 
     # Test openConfig() when the config file isn't set up correctly and there aren't enough lines -> triggering an index error
-    @patch("builtins.open", new_callable=mock_open, read_data="line0\n/flights-dir\n")  # Only two lines
+    @patch(
+        "builtins.open", new_callable=mock_open, read_data="line0\n/flights-dir\n"
+    )  # Only two lines
     @patch.dict(os.environ, {"CONFIG_FILE": "dummy_config"})
-
     def test_open_config_index_error(self, mock_file):
         from ceda_flight_pipeline.cli import openConfig
+
         with patch("builtins.print") as mock_print:
             root, archive = openConfig()
         # Check if it returns empty strings as expected
@@ -49,7 +56,7 @@ class TestFlightUpdate(unittest.TestCase):
 
     # Test moveOldFiles() when files have to be moved
     def test_move_old_files_move_mode(self):
-        commands = []   # initialising empty list where future os commands will be added
+        commands = []  # initialising empty list where future os commands will be added
 
         # Capture shell commands by os.system
         def check_cmnds(cmd):
@@ -62,19 +69,20 @@ class TestFlightUpdate(unittest.TestCase):
 
         with patch("os.system", check_cmnds):
             from ceda_flight_pipeline.cli import moveOldFiles
+
             moveOldFiles(root, archive, files)
 
         # Expected operations for each test file from move function
         expected = [
             f"mv /flights-dir/test.json /archive/test.json",
-            f"mv /flights-dir/file2.json /archive/file2.json"
+            f"mv /flights-dir/file2.json /archive/file2.json",
         ]
         # Check the commands are as expected
         self.assertEqual(commands, expected)
 
     # Test moveOldFiles() when archive mode is DELETE (files should be deleted)
     def test_move_old_files_delete_mode(self):
-        commands = []   # initialising empty list where future os commands will be added
+        commands = []  # initialising empty list where future os commands will be added
 
         # Capture shell commands by os.system
         def check_cmnds(cmd):
@@ -87,16 +95,14 @@ class TestFlightUpdate(unittest.TestCase):
 
         with patch("os.system", check_cmnds):
             from ceda_flight_pipeline.cli import moveOldFiles
+
             moveOldFiles(root, archive, files)
 
         # Expected delete operations for each file
-            
-        expected = [
-            f"rm /flights-dir/test.json",
-            f"rm /flights-dir/file2.json"
-        ]
 
-        self.assertEqual(commands, expected) # check correct
+        expected = [f"rm /flights-dir/test.json", f"rm /flights-dir/file2.json"]
+
+        self.assertEqual(commands, expected)  # check correct
 
 
 # test runner
