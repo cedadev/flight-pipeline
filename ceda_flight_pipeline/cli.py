d2cc0fdfcf599c2a5b80cbd3bd8f757fe712e20a
ioana.circu@stfc.ac.uk <ioana.circu@stfc.ac.uk>
2025-04-10 13:15:37 +0100

    moved functions from flight_update to cli.py

--- ceda_flight_pipeline/cli.py
+++ ceda_flight_pipeline/cli.py
@@ -6,9 +6,15 @@ __copyright__ = "Copyright 2025 United Kingdom Research and Innovation"
 import click
 import sys
 
-from ceda_flight_pipeline.flight_update import addFlights, updateFlights, openConfig
+#from ceda_flight_pipeline.flight_update import addFlights, updateFlights, openConfig, reindex
+from ceda_flight_pipeline.logger import logger
+import logging
+import os
+from ceda_flight_pipeline.flight_client import ESFlightClient
 
-root, archive = openConfig()
+IS_FORCE = True
+VERB = True
+
 
 # Helper function for converting string to boolean
 def str2bool(v):
@@ -18,23 +24,124 @@ def str2bool(v):
     """
     return v.lower() in ("y", "yes", "true", "t", "1")
 
+def openConfig():
+    """
+    Function to open configuration file and initialise paths to relevant directories.
+    
+    Returns: 
+        1. Path to flights to be pushed to ElasticSearch
+        2. Path to directory for moving written flights
+        3. Path to logging file
+    """
+
+    if VERB:
+        #print('> (1/6) Opening Config File')
+        logger.info('> (1/6) Opening Config File')
+
+    f = open(os.environ.get("CONFIG_FILE"),'r')
+    content = f.readlines()
+    f.close()
+    try:
+        return content[1].replace('\n',''), content[3].replace('\n','')
+    except IndexError:
+        logger.error('One or both paths missing from the dirconfig file')
+        print('Error: One or both paths missing from the dirconfig file - please fill these in')
+        return '',''
+
+def moveOldFiles(rootdir, archive, files):
+    """
+    Move the written files from the root directory given in the config file to the archive
+
+    If keyword DELETE given instead of archive then the flight records will be deleted after being pushed
+    """
+
+    # Move the written files from rootdir to the archive
+    if archive != 'DELETE':
+        for file in files:
+            path = os.path.join(rootdir, file.split('/')[-1])
+            new_path = os.path.join(archive, file.split('/')[-1])
+            os.system('mv {} {}'.format(path, new_path))
+    else:
+        for file in files:
+            path = os.path.join(rootdir, file.split('/')[-1])
+            os.system('rm {}'.format(path))
+
+
+def addFlights(rootdir, archive, repush=False):
+    """
+    Initialising connection with ElasticSearch Flight Client and pushing flights to new location.
+
+    Calling moveOldFiles() to delete flight records before exiting.
+    """
+
+    checked_list = []
+
+    # ES client to determine array of ids
+    if VERB:
+        #print('> (2/6) Setting up ES Flight Client')
+        logger.info('> (2/6) Setting up ES Flight Client')
+    if repush:
+        files_list = os.listdir(archive)
+        fclient = ESFlightClient(archive, os.environ.get('SETTINGS_FILE', None))
+    else:
+        files_list = os.listdir(rootdir)
+        fclient = ESFlightClient(rootdir, os.environ.get('SETTINGS_FILE', None))
+
+    # All flights ok to repush - handled by new client.
+    checked_list = list(files_list)
+
+    # Push new flights to index
+    if VERB:
+        #print('> (4/6) Identified {} flights'.format(len(checked_list)))
+        logger.info('> (4/6) Identified {} flights'.format(len(checked_list)))
+    if len(checked_list) > 0:
+        fclient.push_flights(checked_list)
+        if VERB:
+            #print('> (5/6) Pushed flights to ES Index')
+            logger.info('> (5/6) Pushed flights to ES Index')
+        if not repush:
+            moveOldFiles(rootdir, archive, checked_list)
+        if VERB:
+            #print('> (6/6) Removed local files from push directory')
+            logger.info('> (6/6) Removed local files from push directory')
+    else:
+        if VERB:
+            #print('> Exiting flight pipeline')
+            logger.info('> Exiting flight pipeline')
+
+    # Move old records into an archive directory
+
+def updateFlights(update):
+    """
+    Update flights using resolve_link() from flightpipe/flight_client module.
+    """
+    from ceda_flight_pipeline import updaters
+    fclient = ESFlightClient('', os.environ.get('SETTINGS_FILE', None))
+    updaters[update](fclient)
+
+def reindex(new_index):
+    """
+    Running a re-index using the source and destination from settings_file.
+    """
+    fclient = ESFlightClient('', os.environ.get('SETTINGS_FILE', None))
+    fclient.reindex(new_index)
+
+root, archive = openConfig()
+
 @click.group()
 def main():
     """Command Line Interface for flight update"""
     pass
 
 @main.command()
-@click.option('--logging', default="y", type=bool, help='Enable logging (True/False)', prompt='Enable logging (y/n)')
-@click.option('--enable_console_logging', default="n", type=bool, help='Log to console (True/False)', prompt='Log to console (y/n)')
 @click.option('--archive_path', default=archive, required=True, help='Set archive path', prompt='Set archive path')
 @click.option('--flights_dir', default=root, required=True, help='Set path where flights will be pushed', prompt='Set path to flights to be pushed')
-@click.option('--add_mode', default="y", required=True, type=str, help='Set mode to just flights', prompt='Set mode to add flights (y/n)')
-@click.option('--update_mode', default="n", required=True, type=str, help='Set mode to update flights', prompt='Set update mode (y/n)')
-@click.option('--update_id', default="n", required=True, type=str, help='Update based on specific id', prompt='Flight id to update')
+@click.option('--add_mode', default="y", type=str, help='Set mode to just flights', prompt='Set mode to add flights (y/n)')
+@click.option('--update_mode', default="n", type=str, help='Name of script in updates/ to use', prompt='Name of script to update')
+@click.option('--update_id', default="n", type=str, help='New elasticsearch index to move to', prompt='Flight id to update')
 
 
-
-def flight_update(logging, enable_console_logging, archive_path, flights_dir, add_mode, update_mode, update_id):
+def flight_update(archive_path, flights_dir, add_mode, update_mode, update_id):
     """
     Main function running the flight_update.py script based on the given command line parameters
     """
@@ -57,7 +164,10 @@ def flight_update(logging, enable_console_logging, archive_path, flights_dir, ad
             addFlights(flights_dir, archive_path, repush=REPUSH)
 
     elif update_mode:
-        updateFlights(update_id)
+        updateFlights(update_mode)
+
+    elif update_id:
+        reindex(update_id)
 
     else:
         print('Error: Mode unrecognized. Please choose either add or update.')
